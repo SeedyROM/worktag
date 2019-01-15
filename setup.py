20ec4a304b292a048f0f70436677fd16a22fd961
Zack Kollar <rallokkcaz@gmail.com>
2019-01-15 03:32:28 -0800

    Working on setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+# flake8: noqa
 from setuptools import setup
 
-setup(
-    
+setup(name='worktag',
+    version='0.0.1',
+    description='A simple workspace tagging and retrieval tool',
+    url='http://github.com/SeedyROM/worktag',
+    author='Zack Kollar',
+    author_email='me@seedyrom.io',
+    license='MIT',
+    packages=['worktag'],
+    zip_safe=False
 )
